04d3d356525c33ffd115d71f34f22eb23918b544
McElroy, Kerensa (A&F, Black Mountain) <Kerensa.Mcelroy@csiro.au>
2020-03-25 10:46:49 +1100

    initial commit

---
+++ scripts/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export BIG=/datasets/work/AF_CROWN_RUST_WORK/2020-02-28_GWAS
+
+
+#genome files
+
+wget -nc -P ${BIG}/data ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/fungi/Puccinia_coronata/latest_assembly_versions/GCA_002873275.1_ASM287327v1
+
