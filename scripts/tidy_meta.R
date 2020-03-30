2cc589ac77fde3608d22f96ec869d63d58cd86de
McElroy, Kerensa (A&F, Black Mountain) <Kerensa.Mcelroy@csiro.au>
2020-03-30 13:02:28 +1100

    read in and clean SA data

--- scripts/tidy_meta.R
+++ scripts/tidy_meta.R
@@ -18,9 +18,22 @@ headers <- data.frame(t(headers))
 
 column_labels <- headers %>% summarize_all(str_c, collapse = "_")
 
-headers %>% unname(unlist(column_labels[1,]))
+headers <-  unname(unlist(column_labels))
 
-#now read the rest of the file
+#now read the rest of the file ,'state','sample_no','pustule'
 lanes <- read_excel(path = 'data/Figueroa_Project_006_NovaSeq_Summary.xlsx',
                       skip = 11, col_names = headers) %>%
-  clean_names()
+  clean_names() %>%
+  separate(sample, c('year', 'remainder'),
+           remove = FALSE, sep = "[:alpha:]")
+
+#read in South African data and clean names to match lane df
+
+SA <- read_excel(path = 'data/BoshoffWHP_OatCrownRust_SA2018_1.xlsx', skip = 1) %>%
+  clean_names() %>%
+  mutate(sample_id = str_replace(sample_id, '/', '-')) %>%
+  mutate(sample_id = str_replace(sample_id, ' ', ''))
+
+setdiff(SA$sample_id, lanes$sample)
+
+#Pca75 not in lanes df. PCA84-1-1 ambiguous, awaiting further info
