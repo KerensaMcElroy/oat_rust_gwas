2a898e54c35a0ab621940e0a38a5399314a69f6c
McElroy, Kerensa (A&F, Black Mountain) <Kerensa.Mcelroy@csiro.au>
2020-04-22 10:03:07 +1000

    correcting sample names

--- scripts/tidy_meta.R
+++ scripts/tidy_meta.R
@@ -25,28 +25,27 @@ lanes <- read_excel(path = 'data/Figueroa_Project_006_NovaSeq_Summary.xlsx',
                       skip = 11, col_names = headers) %>%
   clean_names() %>%
   separate(sample, c('year', 'remainder'),
-           remove = FALSE, sep = "[:alpha:]") %>%
+           remove = FALSE, sep = "[:alpha:]+") %>%
   mutate(year = case_when(as.numeric(year) <= 20 ~ str_c('20', year),     #reformat for later coalesce with SA
-                          as.numeric(year) > 20 & as.numeric <= 99 ~ str_c('19', year))) %>%
-  mutate(year = as.numeric(year)) 
+                          as.numeric(year) > 20 & as.numeric(year) <= 99 ~ str_c('19', year))) %>%
+  mutate(year = as.numeric(year)) %>%
+  mutate(sample = if_else(sample == "PCA84-1-1_B03", "Pca84-1_H07_S85", sample)) %>%
+  mutate(sample = if_else(sample == "PCA84-1-1_H07", "PCA84-1-1_B03_S102", sample))
 
 #read in South African data and clean names to match lane df
 
 SA <- read_excel(path = 'data/BoshoffWHP_OatCrownRust_SA2018_1.xlsx', skip = 1) %>%
   clean_names() %>%
   mutate(sample_id = str_replace(sample_id, '/', '-')) %>%
-  mutate(sample_id = str_replace(sample_id, ' ', ''))
+  mutate(sample_id = str_replace(sample_id, ' ', '')) 
 
+#manually correcting sample names. See email chain from Eva.
+SA[11,2] <- "Pca84-1_H07_S85"
+SA[32,2] <- "PCA84-1-1_B03_S102"
 setdiff(SA$sample_id, lanes$sample)
 
 #Pca75 not in lanes df. PCA84-1-1 ambiguous, awaiting further info
-
-merged <- full_join(lanes, SA, by = c("sample" = "sample_id")) %>%
-  mutate(year = coalesce(year.x, year.y)) %>%
-  select(-year.x, -year.y) %>%
-  mutate(pustule = case_when(str_detect(sample, "[:digit:]{2}[:alpha:]{2}[:digit:]+-[123]$") ~
-                               str_extract(sample, '[123]$')))
-
+#Update 21.4.19: ignoring PCA84-1-1 for now as it is a SA isolate.
 
 #Import and clean phenotype data
 #note: scores are on scale from 0 to 9, and are the mean of 
@@ -62,4 +61,15 @@ phenos <- read_excel(path = 'data/2016_Pca_isolates_phenotype_completeset.xlsx',
   rownames_to_column(var = "sample") %>%
   as_tibble()
 
-  
+#merging data so far
+
+merged <- full_join(lanes, SA, by = c("sample" = "sample_id")) %>%
+  mutate(year = coalesce(year.x, year.y)) %>%
+  select(-year.x, -year.y) %>%
+  mutate(pustule = case_when(str_detect(sample, "[:digit:]{2}[:alpha:]{2}[:digit:]+-[123]$") ~
+                               str_extract(sample, '[123]$'))) %>%
+  mutate(state = case_when(str_detect(sample,"[:digit:]{2}[:alpha:]{2,4}[:digit:]*-[:digit:]+$") ~
+                             str_extract(sample,'[:alpha:]{2}'))) %>% 
+  mutate(buckthorn = if_else(str_detect(sample,"[:digit:]{2}[:alpha:]{2}BT-[:digit:]+$"), TRUE, FALSE)) %>%
+  full_join(phenos, by = "sample")
+
