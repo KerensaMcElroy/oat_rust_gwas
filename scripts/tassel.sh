7fa2ca5e19dc6619c0e3b4155db33bed47098e03
McElroy, Kerensa (A&F, Black Mountain) <Kerensa.Mcelroy@csiro.au>
2020-07-10 22:22:25 +1000

    mlm no compression

--- scripts/tassel.sh
+++ scripts/tassel.sh
@@ -7,11 +7,11 @@ module load tassel/5.2.63
 
 #run_pipeline.pl -Xms10g -Xmx10g -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/all_isolates_12SD80.biallelic.maf_missing_filter.sorted.vcf -PrincipalComponentsPlugin -covariance true -endPlugin -export /scratch1/mce03b/2020-02-28_GWAS/data/all_isolates_12SD80.biallelic.maf_missing_filter.sorted.pca
 
-#run_pipeline.pl -Xmx10g -Xms10g -fork1 -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/all_isolates_12SD80.biallelic.maf_missing_filter.sorted.vcf -filterAlign -filterAlignMinFreq 0.05 -fork2 -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/pheno_complete_avg_selected.txt -fork3 -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/all_isolates_12SD801.txt -combine4 -input1 -input2 -input3 -intersect -fork5 -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/all_isolates_12SD80.biallelic.maf_missing_filter.sorted.txt -combine6 -input5 -input4 -mlm -mlmVarCompEst P3D -mlmCompressionLevel Optimum -mlmMaxP 0.01 -export /scratch1/mce03b/2020-02-28_GWAS/data/mlm
+run_pipeline.pl -Xmx10g -Xms10g -fork1 -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/all_isolates_12SD80.biallelic.maf_missing_filter.sorted.vcf -fork2 -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/pheno_complete_avg_selected.txt -fork3 -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/all_isolates_12SD801.txt -combine4 -input1 -input2 -input3 -intersect -fork5 -importGuess /scratch1/mce03b/2020-02-28_GWAS/data/all_isolates_12SD80.biallelic.maf_missing_filter.sorted.txt -combine6 -input5 -input4 -mlm -mlmVarCompEst EachMarker -mlmCompressionLevel None -export /scratch1/mce03b/2020-02-28_GWAS/data/mlm_nocomp
 
-grep 'Pc35' /scratch1/mce03b/2020-02-28_GWAS/data/mlm7.txt > Pc35.txt
-awk '$1 == "Pc35" { print $7 }' /scratch1/mce03b/2020-02-28_GWAS/data/mlm7.txt > Pc35_pvalues.txt
-grep -vwE "(NaN)" Pc35_pvalues.txt > Pc35_pvalues_only.txt
+#grep 'Pc63' /scratch1/mce03b/2020-02-28_GWAS/data/mlm7.txt > Pc63.txt
+awk '$1 == "Pc63" { print $7 }' /scratch1/mce03b/2020-02-28_GWAS/data/mlm7.txt > Pc63_pvalues.txt
+grep -vwE "(NaN)" Pc63_pvalues.txt > Pc63_pvalues_only.txt
 
 
 
