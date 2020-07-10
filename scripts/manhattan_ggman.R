093ddc537fef78a4ce75c130568b3223cbbe9efd
Kerensa McElroy <kerensa.mcelroy@csiro.au>
2020-07-10 17:10:07 +1000

    manhattan

--- scripts/manhattan_ggman.R
+++ scripts/manhattan_ggman.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(qvalue)
+library(ggman)
 
 p <- scan("scripts/Pc35_pvalues_only.txt")
 qobj <- qvalue_truncp(p)
@@ -11,3 +12,9 @@ p <- scan("scripts/Pc63_pvalues_only.txt")
 qobj <- qvalue_truncp(p)
 max(qobj$pvalues[qobj$qvalues <= 0.05])
 min(qobj$qvalues)
+
+0.05/860632
+
+gwas <- read_tsv('scripts/Pc63.txt', col_names = FALSE)
+gwas
+ggman(gwas, snp='X2', bp='X4', chrom='X3',pvalue='X7')
