a2550b54371cbaa4487986ecee1745f0fad49268
McElroy, Kerensa (A&F, Black Mountain) <Kerensa.Mcelroy@csiro.au>
2020-08-13 11:48:51 +1000

    manhattan plots

--- scripts/manhattan_ggman.R
+++ scripts/manhattan_ggman.R
@@ -2,7 +2,15 @@ library(tidyverse)
 library(qvalue)
 library(ggman)
 
-p <- scan("scripts/Pc35_pvalues_only.txt")
+pc_stats <- read_tsv(file='analysis/tassel/12NC29/interim/binary/diff_0/mlm_nocomp2.txt')
+
+p_vals <- pc_stats %>% 
+  filter(!is.na(p) & !is.nan(p)) %>%
+  select(p)
+
+oat_diff <- pc_stats$Trait[1]
+
+#p <- scan("scripts/Pc35_pvalues_only.txt")
 qobj <- qvalue_truncp(p)
 max(qobj$pvalues[qobj$qvalues <= 0.05])
 qobj
